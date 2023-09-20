86fcc5320e185f4962cb17adf843c78a52d7d1f4
Dimonti <d1mont1@mail.ru>
2023-09-20 18:52:30 +0600

    Добавьте файлы проекта.

---
+++ dotNET-module-1-practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задача 1
+        Console.Write("Введите ваше имя: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Введите вашу фамилию: ");
+        string lastName = Console.ReadLine();
+
+        string fullName = $"{firstName} {lastName}";
+        Console.WriteLine($"Приветствую тебя - {fullName}");
+
+        // Задача 2
+        Console.WriteLine("Введите несколько чисел через пробел:");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        int sum = 0;
+        foreach (string numberStr in numbers)
+        {
+            if (int.TryParse(numberStr, out int number))
+            {
+                sum += number;
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка: '{numberStr}' не является числом и будет проигнорировано.");
+            }
+        }
+
+        Console.WriteLine($"Сумма введенных чисел: {sum}");
+
+        Console.WriteLine("Нажмите Enter, чтобы завершить...");
+        Console.ReadLine();
+    }
+}
